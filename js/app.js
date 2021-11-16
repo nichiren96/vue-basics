431e122acf9fbbf84c5e33c4679550d5fd004f53
Roel Tombozafy <rtombozafy96@gmail.com>
2021-11-16 17:57:43 +0300

    Dynamic classes

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
             url: 'https://www.google.com',
             showPlayers: true,
             players: [
-                { name:'Lionel Messi', club: 'PSG', img: 'assets/1.jpg' },
-                { name:'Memphis Depay', club: 'FC Barcelona', img: 'assets/2.png' },
-                { name:'Cristiano Ronaldo', club: 'Man United', img: 'assets/3.jpg'},
+                { name:'Lionel Messi', club: 'PSG', img: 'assets/1.jpg', wonUcl: true },
+                { name:'Memphis Depay', club: 'FC Barcelona', img: 'assets/2.png', wonUcl: false },
+                { name:'Cristiano Ronaldo', club: 'Man United', img: 'assets/3.jpg', wonUcl: true},
             ]
         }
     },
