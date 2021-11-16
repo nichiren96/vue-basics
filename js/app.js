39df3333be92350fdf686b87b2d24426dfff470c
Roel Tombozafy <rtombozafy96@gmail.com>
2021-11-16 16:52:36 +0300

    Mouse Events

--- js/app.js
+++ js/app.js
@@ -4,13 +4,27 @@ const app = Vue.createApp({
             showPlayers: true,
             name: 'Lionel Messi',
             club: 'PSG',
-            age: 34
+            age: 34,
+            x: 0,
+            y: 0
         }
     },
 
     methods: {
         toggleShowPlayers() {
             this.showPlayers = !this.showPlayers
+        },
+
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
